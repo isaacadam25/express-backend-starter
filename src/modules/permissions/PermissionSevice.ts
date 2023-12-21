306bc392d034624b968a3db5074eaa6b766f0285
Issa Adam <isaacadam04@gmail.com>
2023-12-21 19:44:36 +0300

    refactor: add js doc permission services

--- src/modules/permissions/PermissionSevice.ts
+++ src/modules/permissions/PermissionSevice.ts
@@ -3,33 +3,57 @@ import { IPermission } from "@/models/permission/types";
 import { IPermissionBody } from "@/modules/permissions/types";
 
 class PermissionService extends PermissionRepository {
-  // create new permission
+  /**
+   * @description Create new permission
+   *
+   * @param {string} payload - The permission details payload
+   * @returns {Promise<IPermission | null>} A promise that resolves to the retrieved permission or null.
+   */
   static createPersmission = (
     payload: IPermissionBody
   ): Promise<IPermission | null> => {
     return this.create(payload);
   };
 
-  // get permission by generic name
+  /**
+   * @description Get permission by generic name
+   *
+   * @param {string} genericName - The permission generic name
+   * @returns {Promise<IPermission | null>} A promise that resolves to the retrieved permission or null.
+   */
   static getByGenericName = (
     genericName: string
   ): Promise<IPermission | null> => {
     return this.getOneByQuery({ generic_name: genericName });
   };
 
-  // get permission by display name
+  /**
+   * @description Get permission by display name
+   *
+   * @param {string} displayName - The permission display name
+   * @returns {Promise<IPermission | null>} A promise that resolves to the retrieved permission or null.
+   */
   static getByDisplayName = (
     displayName: string
   ): Promise<IPermission | null> => {
     return this.getOneByQuery({ display_name: displayName });
   };
 
-  // get permission by its coresponding id
+  /**
+   * @description Get permission by its corresponding id
+   *
+   * @param {string} id - The permission ID
+   * @returns {Promise<IPermission | null>} A promise that resolves to the retrieved permission or null.
+   */
   static getPermissionById = (id: string): Promise<IPermission | null> => {
     return this.getById(id);
   };
 
-  // retrieve all permissions from database
+  /**
+   * @description Retrieve all permissions from database
+   *
+   * @returns {Promise<IPermission | null>} A promise that resolves to the retrieved permission or null.
+   */
   static getAllPermissions = (): Promise<IPermission[] | null> => {
     return this.getAll();
   };
