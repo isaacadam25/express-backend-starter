f764f989dae0603159771392e252a759fc49239d
Issa Adam <isaacadam04@gmail.com>
2023-12-22 12:21:13 +0300

    refactor: add js doc role utils

--- src/modules/roles/RoleUtils.ts
+++ src/modules/roles/RoleUtils.ts
@@ -1,26 +1,38 @@
-import { IRole } from "@/models/role/types";
-import { IRoleBody, IRolePayload } from "@/modules/roles/types";
 import { map } from "lodash";
 
+// import required types
+import { IRole } from "@/models/role/types";
+import { IRoleBody, IRolePayload } from "@/modules/roles/types";
+
 class RoleUtils {
-  // format role to database query format
-  static formatRole = (role: IRolePayload): IRoleBody => {
+  /**
+   * @description Format role to database format
+   *
+   * @param {IRolePayload} payload - The role data from the payload.
+   * @returns {IRoleBody} The formatted role data for saving to database.
+   */
+  static formatRole = (payload: IRolePayload): IRoleBody => {
     const formattedRole: IRoleBody = {
-      role_name: role.roleName,
-      description: role.description,
-      permissions: role.permissions,
+      role_name: payload.roleName,
+      description: payload.description,
+      permissions: payload.permissions,
     };
 
     return formattedRole;
   };
 
-  // sanitize role to human readable format
-  static sanitizeRole = (role: IRole): any => {
+  /**
+   * @description Sanitize role to human readable format
+   *
+   * @param {IRolePayload} payload - The role data from the payload.
+   * @returns {IRoleBody} The formatted role data for saving to human readable format.
+   */
+  static sanitizeRole = (payload: IRole): any => {
     return {
-      id: role._id,
-      roleName: role.role_name,
-      description: role.description,
-      permisssions: map(role.permissions, (permission) => {
+      id: payload._id,
+      roleName: payload.role_name,
+      description: payload.description,
+      permisssions: map(payload.permissions, (permission) => {
         return {
           id: permission._id,
           genericName: permission.generic_name,
