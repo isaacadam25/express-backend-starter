56eb29396f15f6a71140cad03131c52934ee9921
Issa Adam <isaacadam04@gmail.com>
2023-12-22 12:33:51 +0300

    refactor: add js doc encryption classes

--- src/utils/helpers/Encryption.ts
+++ src/utils/helpers/Encryption.ts
@@ -3,13 +3,24 @@ import bcrypt from "bcryptjs";
 const saltRounds = 10;
 
 class Encryption {
-  // -> hash function
+  /**
+   * @description An encryption function for password encryption
+   *
+   * @param {string} value - The value to encrypt
+   * @returns {string} - A Promise string indicating encrypted value.
+   */
   static encrypt = async (value: string): Promise<string> => {
     const encryptedValue = await bcrypt.hash(value, saltRounds);
     return encryptedValue;
   };
 
-  // -> decrypt value
+  /**
+   * @description An dencryption function for password encryption
+   *
+   * @param {string} value - The value to encrypt
+   * @param {string} hash - The harsh value to dencrypt
+   * @returns {string} - A Promise string indicating dencrypted value.
+   */
   static decrypt = async (value: string, hash: string): Promise<boolean> => {
     const dencryptedValue = await bcrypt.compare(value, hash);
     return dencryptedValue;
